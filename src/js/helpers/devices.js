5716807cd9bb26a0834a243bae75d148413e40d5
inotom <wdf7322@yahoo.co.jp>
2018-09-09 17:03:00 +0900

    Remove unused files

--- src/js/helpers/devices.js
+++
@@ -1,9 +0,0 @@
-/** @prettier */
-
-export const isMobile = (width = 640) => {
-  return window.matchMedia(`(max-width: ${width}px)`).matches;
-};
-
-export const isTouchable = () => {
-  return window.ontouchstart === null;
-};
--- src/js/helpers/envs.js
+++
@@ -1,9 +0,0 @@
-/** @prettier */
-
-export const isDevs = () => {
-  return process.env.NODE_ENV === 'development';
-};
-
-export const isProd = () => {
-  return process.env.NODE_ENV === 'production';
-};
